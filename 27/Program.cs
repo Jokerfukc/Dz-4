4c3005ce4d32f281e3d81028a07b86b932ef13a6
Joker <aml97@yandex.ru>
2023-01-31 18:40:33 +0300

    dz#4

---
+++ 27/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum=0;
+for (int i=99; i>=0; i--)
+{
+    sum+=number/(int)Math.Pow(10.0,i);
+    number=number%(int)Math.Pow(10.0,i);
+}
+Console.WriteLine(sum);
